c7b80fbee4ccded1ba9687c1a9e9af8729942101
Jose Flores <jop0693@gmail.com>
2024-03-22 07:00:56 -0600

    cluster of webservers

--- main.tf
+++ main.tf
@@ -40,3 +40,46 @@ output "public_ip" {
   value       = aws_instance.example.public_ip
   description = "The public IP of the web server"
 }
+
+resource "aws_launch_configuration" "example" {
+    image_id = "ami-0fb653ca2d3203ac1"
+    instance_type = "t2.micro"
+    security_groups = [aws_security_group.instance.id]
+
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello from aws launch configuration" > index.html
+                nohup busybox httpd -f -p ${var.server_port} &
+                EOF
+    
+    lifecycle {
+        create_before_destroy = true
+    }
+}
+
+resource "aws_autoscaling_group" "example" {
+    launch_configuration = aws_launch_configuration.example.id
+    vpc_zone_identifier = data.aws_subnet.default.ids
+
+    min_size = 2
+    max_size = 10
+
+    tag {
+        key = "Name"
+        value = "terraform-asg-example"
+        propagate_at_launch = true
+    }
+}
+
+data  "aws_vpc" "default" {
+    default = true
+}
+
+data "aws_subnet" "default" {
+    filter {
+        name = "vpc-id"
+        values = [data.aws_vpc.default.id]
+    }
+}
+
+
