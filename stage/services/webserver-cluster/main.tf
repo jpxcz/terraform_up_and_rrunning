1c48930b98ed056368d2c747d6be909476ff12c5
Jose Flores <jop0693@gmail.com>
2024-03-27 18:00:39 -0600

    modules working

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -1,176 +1,21 @@
-terraform {
-  backend "s3" {
-    bucket = "jflores-terraform-up-and-running-state"
-    key    = "stage/services/webserver-cluster/terraform.tfstate"
-    region = "us-west-2"
-
-    dynamodb_table = "terraform-up-and-running-locks"
-    encrypt        = true
-  }
-}
-
 provider "aws" {
   region = "us-west-2"
 }
 
-data "terraform_remote_state" "db" {
-  backend = "s3"
+module "webserver_cluster" {
+  source = "../../../modules/services/webserver-cluster"
 
-  config = {
-    bucket = "jflores-terraform-up-and-running-state"
-    key    = "stage/data-storage/mysql/terraform.tfstate"
-    region = "us-west-2"
-  }
+  cluster_name           = "webservers-stage"
+  db_remote_state_bucket = "jflores-terraform-up-and-running-state"
+  db_remote_state_key    = "stage/data-storage/mysql/terraform.tfstate"
 }
 
-resource "aws_instance" "example" {
-  ami                    = "ami-08116b9957a259459"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
-  user_data              = <<-EOF
-                #!/bin/bash
-                echo "Hello world!" > index.html
-                nohup busybox httpd -f -p ${var.server_port} &
-                EOF
+resource "aws_security_group_rule" "allow_testing_inbound" {
+  type              = "ingress"
+  security_group_id = module.webserver_cluster.alb_security_group_id
 
-  user_data_replace_on_change = true
-
-  tags = {
-    Name = "terraform-example"
-  }
+  from_port   = 12345
+  to_port     = 12345
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
 }
-
-resource "aws_security_group" "instance" {
-  name = "terraform-example-intance"
-
-  ingress {
-    from_port   = var.server_port
-    to_port     = var.server_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_launch_configuration" "example" {
-  image_id        = "ami-08116b9957a259459"
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.instance.id]
-
-  # Render the User Data script as a template
-  user_data = templatefile("user-data.sh", {
-    server_port = var.server_port
-    db_address  = data.terraform_remote_state.db.outputs.address
-    db_port     = data.terraform_remote_state.db.outputs.port
-  })
-
-  # Required when using a launch configuration with an auto scaling group.
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_autoscaling_group" "example" {
-  launch_configuration = aws_launch_configuration.example.id
-  vpc_zone_identifier  = data.aws_subnets.default.ids
-
-  target_group_arns = [aws_lb_target_group.asg.arn]
-  health_check_type = "ELB"
-
-  min_size = 2
-  max_size = 10
-
-  tag {
-    key                 = "Name"
-    value               = "terraform-asg-example"
-    propagate_at_launch = true
-  }
-}
-
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
-resource "aws_lb" "example" {
-  name               = "terraform-asg-example"
-  load_balancer_type = "application"
-  subnets            = data.aws_subnets.default.ids
-  security_groups    = [aws_security_group.alb.id]
-}
-
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.example.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  # By default, return a simple 404 page
-  default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "404: page not found"
-      status_code  = "404"
-    }
-  }
-}
-
-resource "aws_security_group" "alb" {
-  name = "terraform-example-alb"
-
-  # Allow inbound HTTP requests
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Allow all outbound requests
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_lb_target_group" "asg" {
-  name     = "terraform-asg-example"
-  port     = var.server_port
-  protocol = "HTTP"
-  vpc_id   = data.aws_vpc.default.id
-
-  health_check {
-    path                = "/"
-    protocol            = "HTTP"
-    matcher             = "200"
-    interval            = 15
-    timeout             = 3
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
-}
-
-resource "aws_lb_listener_rule" "asg" {
-  listener_arn = aws_lb_listener.http.arn
-  priority     = 100
-
-  condition {
-    path_pattern {
-      values = ["*"]
-    }
-  }
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.asg.arn
-  }
-}
-
--- stage/services/webserver-cluster/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "server_port" {
-  description = "The port for the server will use for the HTTP request"
-  type        = number
-  default     = 8080
-}
