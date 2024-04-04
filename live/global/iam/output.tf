ea191cf228f31ff894861d928bc029d9e8c4e248
Jose Flores <jop0693@gmail.com>
2024-04-04 10:56:56 -0600

    chapter 5. Loops with the for String Directive

--- live/global/iam/output.tf
+++ live/global/iam/output.tf
@@ -2,3 +2,12 @@ output "user_arns" {
   value       = values(module.users)[*].user_arn
   description = "The ARNs of the created IAM users"
 }
+
+output "for_directive" {
+  value = "%{for name in var.user_names}${name}, %{endfor}"
+}
+
+output "for_directive_index" {
+  value = "%{for i, name in var.user_names}(${i}) ${name}, %{endfor}"
+}
+
